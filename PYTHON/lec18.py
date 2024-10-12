1e98e6dd9069abbdfd87ee34ee16fa4496de8c68
jagatjyoti-01 <jagatjyotidash33@gmail.com>
2024-10-12 22:49:42 +0530

    hh

---
+++ PYTHON/lec18.py
@@ -0,0 +1,38 @@
+# a=6
+# print(a)
+# b=9
+# print(b)
+# c="jagat"
+# g="7"
+# print(g)
+# g=int(g)+9
+
+# print(g)
+
+#a = input("enter your anme ")
+#print(a)
+
+name="jagat jyoti"
+print(name[0:3])
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(name.swapcase())
+print(name.center(20,"*"))
+print(name.ljust(20,"*"))
+print(name.rjust(20,"*"))
+print(name.count("a"))
+print(name.find("a"))
+print(name.replace("a","b"))
+#print(name.split(" "))
+# print(name.split(","))
+ #print(name.split(","))
+# print(name.startswith("a"))
+# print(name.endswith("i"))
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.isalnum())
+# print(name.islower())
+
+
